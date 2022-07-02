24f32f6766f3543f10111205fa5f4a81a5fcdaf3
Miguel Ayllón <ayllondark@gmail.com>
2022-07-01 22:00:39 -0500

    cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const user = {
   }
   const navigation = [
     { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
+    { name: 'Marvel', href: '#', current: false },
     { name: 'Projects', href: '#', current: false },
     { name: 'Calendar', href: '#', current: false },
     { name: 'Reports', href: '#', current: false },

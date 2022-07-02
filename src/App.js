2700d0c95007b6c518cd462342b5e2e6a917b585
Miguel Ayllón <ayllondark@gmail.com>
2022-07-02 01:30:42 -0500

    agregando login amplify a pelis 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
+import { Authenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <>
+    <Authenticator>
     <Navbar />
     <Home />
+    </Authenticator>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import App from './App';
 
-import {Amplify} from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut, AmplifyProvider } from '@aws-amplify/ui-react'; 
+import {Amplify, Auth} from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+//import { AmplifyProvider } from '@aws-amplify/ui-react'; 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <>
-    <AmplifyProvider>
     <App />
-    </AmplifyProvider>
-    </>
 );
 

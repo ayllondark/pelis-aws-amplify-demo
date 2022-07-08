426f2c07380dd9a0b883459b945c88148136c462
Miguel Ayllón <ayllondark@gmail.com>
2022-07-07 19:00:57 -0500

    cargando cambio buscador2

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -73,7 +73,7 @@ const Listado = ({listadoState, setListadoState}) => {
     <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{peli.titulo}</h5>
     <span className="text-sm text-gray-500 dark:text-gray-400">{peli.descripcion}</span>
     <div key={peli.id} className="flex mt-4 space-x-3 lg:mt-6">
-        <a href ='#' className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={ () => setEditar(peli.id)}>Editar</a>
+        {/* <a href ='#' className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={ () => setEditar(peli.id)}>Editar</a> */}
         <a href ='#' className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700" onClick={ () => borrarPeli(peli.id)}>Borrar</a>
     </div>
 
